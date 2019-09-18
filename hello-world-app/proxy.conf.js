f6c57309cf99c21d95a55c79f61e1f809b43e2c5
RodrigoFrancaBR <RodrigoFrancaBR@users.noreply.github.com>
2019-09-17 22:16:41 -0300

    implementando selenium

--- hello-world-app/proxy.conf.js
+++ hello-world-app/proxy.conf.js
@@ -1,7 +1,8 @@
 const PROXY_CONFIG = [
     {
         context: ['/api'],
-        target: 'http://localhost:3000/',
+        // target: 'http://localhost:3000/',
+        target: 'http://localhost:8080/',
         secure: false,
         logLevel: 'debug',
         pathRewrite: { '^/api': '' }
